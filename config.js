ef39693081599b561a6cf91c1cacc70305f26f3b
SonikWebDesign <nikielenkov@icloud.com>
2026-08-19 03:19:50 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 */
 window.WOODRA_CONFIG = {
   bookingEndpoint: "https://script.google.com/macros/s/AKfycbxM4CvG0r4XhYn_ZcldjQIUvwFAdePpYl5nFZQ8Vbr8lBC-eZ-LBsRKJRR74ivDsUu0/exec",
-  minNights: 1,
+  minNights: 2,
   singleHouseMaxGuests: 8,
   availabilityMonthsAhead: 18
 };
